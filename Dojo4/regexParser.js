ab6a26a300193eff4dddf248b0aee7ea993556bc
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2012-04-21 16:07:56 +0300

    output from 4th Dojo

--- Dojo4/regexParser.js
+++ Dojo4/regexParser.js
@@ -1,3 +1,94 @@
-exports.parse = function () {
-    return 1;
+var detectQuant = function (regex) {
+    var start = regex.indexOf('{');
+    var end = regex.indexOf('}');
+
+    if (start != -1 && end != -1 && start < end) {
+        var qstr = regex.substring(start + 1, end);
+        var num_arr = qstr.split(',');
+
+        if (num_arr.length == 2)
+            return [regex[start - 1], num_arr[0], num_arr[1]];
+    }
+
+    return [];
+};
+
+
+exports.parse = function (regex, inputStr) {
+    var retval = [];
+
+    var quant_arr = detectQuant(regex);
+
+    if (quant_arr.length == 3) {
+        var cnt = 0;
+
+        for (var i = 0; i < inputStr.length; i++) {
+            if (inputStr[i] == quant_arr[0]) {
+                if (cnt == quant_arr[2]) {
+                    retval.push(inputStr.substring(i - cnt, i));
+                    cnt = 0;
+                } else {
+                    cnt++;
+                }
+            } else {
+                if (cnt >= quant_arr[1]) {
+                    retval.push(inputStr.substring(i - cnt, i));
+                }
+                cnt = 0;
+            }
+        }
+
+
+    }
+    else if ("$" == regex[regex.length - 1]) {
+        var ending = regex.split("$")[0];
+        if (inputStr.lastIndexOf(ending) == inputStr.length - (ending.length)) {
+            retval.push(ending);
+        }
+    } else if ("^" == regex[0]) {
+        var beginnig = regex.split("^")[1];
+        if ('' == inputStr.split(beginnig)[0]) {
+            retval.push(beginnig);
+        }
+    } else
+    if ('.' == regex) {
+        retval = inputStr.split('');
+    } else if ("\\d" == regex) {
+        for (var i = 0; i < inputStr.length; i++) {
+            var num = inputStr[i];
+            if (num >= '0' && num <= '9')
+                retval.push(num);
+        }
+    } else if ("\\D" == regex) {
+        for (var i = 0; i < inputStr.length; i++) {
+            var num = inputStr[i];
+            if (!(num >= '0' && num <= '9')) {
+                retval.push(num);
+            }
+        }
+    } else if (regex[regex.length - 1] == '.') {
+        var exactMatch = regex.split('.')[0];
+        var counter = 0;
+        for (var i = 0; i < inputStr.length; i++) {
+
+            if (inputStr[i] == exactMatch[counter]) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter == exactMatch.length) {
+                i++;
+                if (inputStr[i] !== undefined) {
+                    retval.push(exactMatch + inputStr[i]);
+                }
+            }
+        }
+    } else if (regex.length > 1) {
+        var splitArr = inputStr.split(regex);
+        for (var i = 1; i < splitArr.length; i++) {
+            retval.push(regex);
+        }
+    }
+
+    return retval;
 };
--- Dojo4/regexParserSpec.js
+++ Dojo4/regexParserSpec.js
@@ -1,7 +1,82 @@
 var regexParser = require("./regexParser.js");
 
 describe('regex test', function () {
-    it('test', function () {
-        expect(regexParser.parse()).toBe(1);
+    it('when dot then return string as splitted by char to array', function () {
+        var retval = regexParser.parse(".", "qwerty");
+        expect(retval.length).toBe(6);
+        expect(retval[0]).toBe("q");
+        expect(retval[5]).toBe("y");
     });
+    it('when regexp is digit then all digits should be returned as char array', function () {
+        var retval = regexParser.parse("\\d", "q33w4er5ty5");
+        expect(retval.length).toBe(5);
+        expect(retval[0]).toBe("3");
+        expect(retval[4]).toBe("5");
+    });
+
+    it('when regexp is pattern then return array of patterns', function () {
+        var retval = regexParser.parse("as", "ehjfasjfkdjfasasfhfh");
+        expect(retval.length).toBe(3);
+        expect(retval[0]).toBe("as");
+        expect(retval[2]).toBe("as");
+    });
+
+    it('when regexp is pattern and has inclusions in the begin and end then return array of patterns', function () {
+        var retval = regexParser.parse("as", "asehjfasjfkdjfas");
+        expect(retval.length).toBe(3);
+        expect(retval[0]).toBe("as");
+        expect(retval[2]).toBe("as");
+    });
+
+    it('when regexp is pattern and has inclusions in the begin and end then return array of patterns', function () {
+        var retval = regexParser.parse("\\D", "a5bc");
+        expect(retval.length).toBe(3);
+        expect(retval[0]).toBe("a");
+        expect(retval[1]).toBe("b");
+        expect(retval[2]).toBe("c");
+    });
+
+    it('when regexp is pattern (z.) then return array of patterns', function () {
+        var retval = regexParser.parse("z.", "zabczzqz");
+        expect(retval.length).toBe(2);
+        expect(retval[0]).toBe("za");
+        expect(retval[1]).toBe("zz");
+    });
+
+    it('when regexp is pattern (z.) then return array of patterns', function () {
+        var retval = regexParser.parse("5.", "561156");
+        expect(retval.length).toBe(2);
+        expect(retval[0]).toBe("56");
+        expect(retval[1]).toBe("56");
+    });
+
+    it('when regexp is starting with ^ return array of one elem from beginning #1', function () {
+        var retval = regexParser.parse("^q", "qwertyq");
+        expect(retval.length).toBe(1);
+        expect(retval[0]).toBe("q");
+    });
+
+    it('when regexp is starting with ^ return array of one elem from beginning #2', function () {
+        var retval = regexParser.parse("^q", "wertyq");
+        expect(retval.length).toBe(0);
+    });
+
+    it('when regexp is ending  with $ return array of one elem from beginning #1', function () {
+        var retval = regexParser.parse("qq$", "qwertyqq");
+        expect(retval.length).toBe(1);
+        expect(retval[0]).toBe("qq");
+    });
+
+    it('when regexp is ending with $ return array of one elem from beginning #2', function () {
+        var retval = regexParser.parse("q$", "qwerty");
+        expect(retval.length).toBe(0);
+    });
+    it('when regexp have  quantificator {1,2} than char before it should be present at least one time #2', function () {
+        var retval = regexParser.parse("q{1,2}", "qwertyqq6");
+        expect(retval.length).toBe(2);
+        expect(retval[0]).toBe("q");
+        expect(retval[1]).toBe("qq");
+
+    });
+
 });
