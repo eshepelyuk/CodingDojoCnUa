1a378c095050d4684b266df49c9b9f1945c5186f
zaotroy <zaotroy@gmail.com>
2013-06-08 16:10:14 +0300

    Implemented logic for dividing tasks. Usage channels instead arrays

--- Dojo12/src/dojo12/test_dojo12.go
+++ Dojo12/src/dojo12/test_dojo12.go
@@ -3,7 +3,7 @@ package dojo12
 import (
 	"github.com/orfjackal/gospec/src/gospec"
 	. "github.com/orfjackal/gospec/src/gospec"
-	"fmt"
+	//"fmt"
 )
 
 func MySpec(c gospec.Context) {
@@ -13,10 +13,16 @@ func MySpec(c gospec.Context) {
 			actual := GetMaxPowerOf2(value)
 			c.Expect(actual, Equals, expected)
 		})
+
 	c.Specify("Should return sorted array", func() {
 			inputArr := []int{3, 6, 1, 2}
 			expected := []int{1, 2, 3, 6}
-			actual := SortArray(inputArr)
+			inputChannel := make(chan [] int, 4)
+			inputChannel <- inputArr
+
+			actualChan := SortArray(inputChannel)
+			actual := <- actualChan
+
 			c.Expect(len(actual), Equals, len(expected))
 			for i := 0; i < len(actual); i++ {
 				c.Expect(actual[i], Equals, expected[i])
@@ -32,10 +38,20 @@ func MySpec(c gospec.Context) {
 			offset := 0
 			expected := []int{1, 0, 0, 1, 0, 1, 1, 0}
 			doSwap(inputArr, fromPos, toPos, dist, blockSize, offset)
-			fmt.Println(inputArr)
+//			fmt.Println(inputArr)
 			for i := 0; i < len(inputArr); i++ {
 				c.Expect(inputArr[i], Equals, expected[i])
 			}
+		})
+
+	c.Specify("Should divide input array into tasks", func() {
+			inputArr := []int{1, 2, 3, 4, 5, 6, 7}
+			tasksCount := 3
+			expResult := [][]int {{5, 6, 7}, {2, 3, 4}, {0, 1}}
+			actResult := DivideArrayOnTasks(inputArr, tasksCount)
+			c.Expect(actResult[0], ContainsAny, expResult[0])
+			c.Expect(actResult[1], ContainsAny, expResult[1])
+			c.Expect(actResult[2], ContainsAny, expResult[2])
 		})
 }
 
